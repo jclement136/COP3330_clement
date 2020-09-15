5da140c96a91acffd4eafc163b96d9b940f2f1fe
John <jclement136@gmail.com>
2020-09-14 21:33:00 -0400

    minor encrypter changes

--- clement_p1/src/Encrypter.java
+++ clement_p1/src/Encrypter.java
@@ -1,2 +1,30 @@
 public class Encrypter {
+
+    public String encrypt (String value [4]) {
+        stringToInteger();
+        modifyInteger();
+        swapNumbers();
+        return integerToString();
+    }
+
+    public static int stringToInteger(){
+
+        return ;
+    }
+
+    public static int modifyInteger(){
+
+        return ;
+    }
+
+    public static int swapNumbers(){
+
+        return ;
+    }
+
+    public static String integerToString(){
+
+        return ;
+    }
+
 }
