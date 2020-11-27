ec410c1496e892fb7e0ef759581ec65ae7da4224
John <jclement136@gmail.com>
2020-11-26 19:32:56 -0500

    complete contactitem and contactitemtest

--- clement_p5/src/ContactItem.java
+++ clement_p5/src/ContactItem.java
@@ -1,2 +1,125 @@
+import java.time.LocalDate;
+
 public class ContactItem {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String emailAddress;
+
+    public ContactItem(String firstName, String lastName, String phoneNumber, String emailAddress) {
+        if (firstName.isBlank() && lastName.isBlank() && phoneNumber.isBlank() && emailAddress.isBlank())
+            throw new InvalidContactException("A contact item must contain one of the four fields.");
+        else {
+            setFirstName(firstName);
+            setLastName(lastName);
+            setPhoneNumber(phoneNumber);
+            setEmailAddress(emailAddress);
+        }
+    }
+
+    // SETTERS
+    protected void setFirstName(String firstName) {
+        if (firstName == "" && lastName == "" && phoneNumber == "" && emailAddress == "")
+            throw new InvalidContactException("A contact item cannot contain four blank fields.");
+        else
+            this.firstName = firstName;
+    }
+    protected void setLastName(String lastName) {
+        if (firstName == "" && lastName == "" && phoneNumber == "" && emailAddress == "")
+            throw new InvalidContactException("A contact item cannot contain four blank fields.");
+        else
+            this.lastName = lastName;
+    }
+    protected void setPhoneNumber(String phoneNumber) {
+        if (firstName == "" && lastName == "" && phoneNumber == "" && emailAddress == "")
+            throw new InvalidContactException("A contact item cannot contain four blank fields.");
+        else if (isPhoneNumberValid(phoneNumber))
+            this.phoneNumber = phoneNumber;
+        else
+            throw new InvalidPhoneNumberException("Invalid phone number. Format must be 123-456-7890.");
+    }
+    protected void setEmailAddress(String emailAddress) {
+        if (firstName == "" && lastName == "" && phoneNumber == "" && emailAddress == "")
+            throw new InvalidContactException("A contact item cannot contain four blank fields.");
+        else if (isEmailAddressValid(emailAddress))
+            this.emailAddress = emailAddress;
+        else
+            throw new InvalidEmailAddressException("Invalid email address. Format must be email@address.com.");
+    }
+
+    // GETTERS
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+    public String getEmailAddress() {
+        return this.emailAddress;
+    }
+
+    public String toString() {
+        return firstName+ "\n" + lastName + "\n" + phoneNumber + "\n" + emailAddress;
+    }
+
+    // VALIDITY
+    protected boolean isPhoneNumberValid (String phoneNumber) {
+        // checks for valid date format 123-465-7890
+        if (phoneNumber.isBlank())
+            return true;
+
+        else if (phoneNumber.length() != 12) {
+            return false;
+        }
+        // preliminary hyphen check
+        else if (phoneNumber.charAt(3) != '-' || phoneNumber.charAt(7) != '-') {
+            return false;
+        }
+        // loop to check each index. skip hyphens during digit check. return true if entire string passes test.
+        else {
+            for (int i = 0; i < 12; i++) {
+                if (Character.isDigit(phoneNumber.charAt(i))) {
+                    if (i == 3 || i == 7) {
+                        if (phoneNumber .charAt(i) == '-')
+                            continue;
+                        else
+                            return false;
+                    }
+                    if (i == 11)
+                        return true;
+                    else
+                        continue;
+                }
+            }
+        }
+        return false;
+    }
+    protected boolean isEmailAddressValid(String emailAddress) {
+        if (emailAddress.isBlank())
+            return true;
+        else if (emailAddress.indexOf('@') == -1)
+           return false;
+       else
+           return true;
+    }
+
+    // EXCEPTIONS
+    class InvalidContactException extends IllegalArgumentException {
+        public InvalidContactException(String msg) {
+            super(msg);
+        }
+    }
+    class InvalidPhoneNumberException extends IllegalArgumentException {
+        public InvalidPhoneNumberException(String msg) {
+            super(msg);
+        }
+    }
+    class InvalidEmailAddressException extends IllegalArgumentException {
+        public InvalidEmailAddressException(String msg) {
+            super(msg);
+        }
+    }
 }
--- clement_p5/src/ContactItemTest.java
+++ clement_p5/src/ContactItemTest.java
@@ -1,2 +1,102 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ContactItemTest {
+    
+    // Contact item creation
+    @Test
+    public void creationFailsWithAllBlankValues() {
+        assertThrows(ContactItem.InvalidContactException.class, () -> new ContactItem("", "", "", ""));
+    }
+
+    @Test
+    public void creationSucceedsWithBlankFirstName() {
+        ContactItem item = new ContactItem("", "Lastname", "123-456-7890", "email@address.com");
+        assertEquals("", item.getFirstName());
+    }
+
+    @Test
+    public void creationSucceedsWithBlankLastName() {
+        ContactItem item = new ContactItem("Firstname", "", "123-456-7890", "email@address.com");
+        assertEquals("", item.getLastName());
+    }
+
+    @Test
+    public void creationSucceedsWithBlankPhone() {
+        ContactItem item = new ContactItem("Firstname", "Lastname", "", "email@address.com");
+        assertEquals("", item.getPhoneNumber());
+    }
+
+    @Test
+    public void creationSucceedsWithBlankEmail() {
+        ContactItem item = new ContactItem("Firstname", "Lastname", "123-456-7890", "");
+        assertEquals("", item.getEmailAddress());
+    }
+
+    @Test
+    public void creationSucceedsWithNonBlankValues() {
+        assertDoesNotThrow(() -> new ContactItem("Firstname", "Lastname", "123-456-7890", "email@address.com"));
+    }
+
+    @Test
+    public void creationFailsWithInvalidPhone() {
+        assertThrows(ContactItem.InvalidPhoneNumberException.class, () -> new ContactItem("Firstname", "Lastname", "1234567890", "email@address.com"));
+    }
+
+    @Test
+    public void creationFailsWithInvalidEmail() {
+        assertThrows(ContactItem.InvalidEmailAddressException.class, () -> new ContactItem("Firstname", "Lastname", "123-456-7890", "email"));
+    }
+
+
+    // Editing
+    @Test
+    public void editingFailsWithAllBlankValues() {
+        ContactItem contact = new ContactItem("Firstname", "", "", "");
+        assertThrows(ContactItem.InvalidContactException.class, () -> contact.setFirstName(""));
+    }
+
+    @Test
+    public void editingSucceedsWithBlankFirstName() {
+        ContactItem contact = new ContactItem("", "Lastname", "123-456-7890", "email@address.com");
+        contact.setFirstName("Firstname");
+        assertEquals("Firstname", contact.getFirstName());
+    }
+
+    @Test
+    public void editingSucceedsWithBlankLastName() {
+        ContactItem contact = new ContactItem("Firstname", "", "123-456-7890", "email@address.com");
+        contact.setLastName("Lastname");
+        assertEquals("Lastname", contact.getLastName());
+    }
+
+    @Test
+    public void editingSucceedsWithBlankPhone() {
+        ContactItem contact = new ContactItem("Firstname", "Lastname", "", "email@address.com");
+        contact.setPhoneNumber("123-456-7890");
+        assertEquals("123-456-7890", contact.getPhoneNumber());
+    }
+
+    @Test
+    public void editingSucceedsWithBlankEmail() {
+        ContactItem contact = new ContactItem("Firstname", "Lastname", "123-456-7890", "");
+        contact.setEmailAddress("email@address.com");
+        assertEquals("email@address.com", contact.getEmailAddress());
+    }
+
+    @Test
+    public void editingSucceedsWithNonBlankValues() {
+        ContactItem contact = new ContactItem("Firstname", "Lastname", "123-456-7890", "email@address.com");
+        contact.setFirstName("Name");
+        assertEquals("Name", contact.getFirstName());
+    }
+
+
+    // Misc.
+    @Test
+    public void testToString() {
+        ContactItem contact = new ContactItem("Firstname", "Lastname", "123-456-7890", "email@address.com");
+        String contactString = contact.toString();
+        assertEquals(contact.toString(), "Firstname\nLastname\n123-456-7890\nemail@address.com");
+    }
 }
